06d72fa8f16ef55952cdde4cf259d93ba791cdef
Artium Wasse <artium.wasse@vimcar.com>
2021-05-30 17:42:11 +0200

    chore(config): update svelte config

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,18 +1,18 @@
 /** @type {import('@sveltejs/kit').Config} */
-const vercel = require("@sveltejs/adapter-vercel");
 const path = require("path");
+const vercel = require("@sveltejs/adapter-vercel");
 
 module.exports = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
-    adapter: vercel(),
     vite: {
       resolve: {
         alias: {
           $assets: path.resolve("./src/assets")
         }
       }
-    }
+    },
+    adapter: vercel()
   }
 };
